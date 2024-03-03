1fce4d10e6594370ad1004338b6c62580a9aacf4
Diya Wadhwani <wadhwani2399@gmail.com>
2024-03-02 23:14:51 -0800

    Added pagination feature

--- shopping-cart-application/src/pages/LandingPage.jsx
+++ shopping-cart-application/src/pages/LandingPage.jsx
@@ -6,24 +6,32 @@ import PropTypes from "prop-types";
 import ProductItem from "../components/ProductItem";
 
 export default function LandingPage() {
+  const [currentPage, setCurrentPage] = useState(1);
   const [products, setProducts] = useState([]);
+  const itemsPerPage = 10;
   //   const [cartItemCount, setCartItemCount] = useState(0);
 
   useEffect(() => {
     const getProducts = async () => {
-      const products = await myFirebase.getProducts();
-      console.log(products);
-      setProducts(products);
+      const allProducts = await myFirebase.getProducts();
+      const start = (currentPage - 1) * itemsPerPage;
+      const end = start + itemsPerPage;
+      const paginatedProducts = allProducts.slice(start, end);
+      setProducts(paginatedProducts);
     };
 
-    // const getCartCount = async () => {
-    //   const cartCount = await myFirebase.fetchCountInCart();
-    //   console.log(cartCount);
-    //   setCartItemCount(cartCount.length);
-    // };
-
     getProducts();
-  }, []);
+  }, [currentPage]);
+
+  // const getCartCount = async () => {
+  //   const cartCount = await myFirebase.fetchCountInCart();
+  //   console.log(cartCount);
+  //   setCartItemCount(cartCount.length);
+  // };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   const onAddToCart = async (product) => {
     console.log("Add to Cart", product.id, product.data);
@@ -37,10 +45,40 @@ export default function LandingPage() {
         <h1>Shopping Cart Application</h1>{" "}
         <Link to="/cart">
           <FaShoppingCart className="header-icon" />
-          {/* <span className="cart-count">{cartItemCount}</span> */}
         </Link>
       </div>
-      <ProductItem products={products} onAddToCart={onAddToCart} />{" "}
+      <ProductItem products={products} onAddToCart={onAddToCart} />
+
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+          <button
+            className="page-link"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            <span aria-hidden="true">«</span>
+          </button>
+        </li>
+        <li className={`page-item ${currentPage === 1 ? "active" : ""}`}>
+          <button className="page-link" onClick={() => handlePageChange(1)}>
+            1
+          </button>
+        </li>
+        <li className={`page-item ${currentPage === 2 ? "active" : ""}`}>
+          <button className="page-link" onClick={() => handlePageChange(2)}>
+            2
+          </button>
+        </li>
+        <li className={`page-item ${currentPage === 4 ? "disabled" : ""}`}>
+          <button
+            className="page-link"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === 3}
+          >
+            <span aria-hidden="true">»</span>
+          </button>
+        </li>
+      </ul>
     </>
   );
 }
